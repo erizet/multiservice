971060482ae2d07e1e0620d45be20c66c459f9dd
erik <erik@test.com>
2022-08-15 13:49:38 +0200

    fetch from ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  useEffect(() => {
+    fetch('http://localhost:5000/weatherforecast')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
